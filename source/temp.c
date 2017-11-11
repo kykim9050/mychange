30200f8bb79d5627392383b53151d19333b4aff0
Bora Youn <qhfk1649@naver.com>
2017-11-11 01:13:52 -0800

    make source temp.c

---
+++ source/temp.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void temp()
+{
+double c;
+
+if(t=1){
+	printf("what '℃ '?\m");
+	scanf("%lf", &c);
+	printf("%lf ℃  is\n%lf ℉ \n%lf K",c, 1.8*c+32, c+273);
+}
+else if(t=2){
+	printf("what '℉ '?\n");
+	scanf("%lf", &c);
+	printf("%lf ℉  is\n%lf ℃ \n%lf K",c, (c-32)/1.8, ((c-32)/1.8)+273);
+}
+else if(t=3({
+	printf("what 'Kel'?\n");
+	scanf("%lf", &c);
+	printf("%lf K is\n%lf ℃ \n%lf ℉ ",c, c-273, ((c-273)*1.8+)32);
+else
+	printf("wrong number!\m");
+} 
