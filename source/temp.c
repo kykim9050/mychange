8c41575682c27374540e292dedbbf011f9c5e531
kykim9050 <kykim9050@naver.com>
2017-11-30 18:41:10 -0800

    optimized temp.c

--- source/temp.c
+++ source/temp.c
@@ -21,10 +21,10 @@ else if(t=2){
 	scanf("%lf", &c);
 	printf("%lf ℉  is\n%lf ℃ \n%lf K",c, (c-32)/1.8, ((c-32)/1.8)+273);
 }
-else if(t=3({
+else if(t=3){
 	printf("what 'Kel'?\n");
 	scanf("%lf", &c);
-	printf("%lf K is\n%lf ℃ \n%lf ℉ ",c, c-273, ((c-273)*1.8+)32);
+	printf("%lf K is\n%lf ℃ \n%lf ℉ ",c, c-273, ((c-273)*1.8+32));
 }
 else
 	printf("wrong number!\n");
