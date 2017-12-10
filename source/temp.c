7c52dc4c8c5e21a86483e13839c02d95ae709c5a
Bora Youn <qhfk1649@naver.com>
2017-12-10 01:30:41 -0800

    finally done2

--- source/temp.c
+++ source/temp.c
@@ -10,18 +10,18 @@ double c;
 printf("type number(1.℃  2.℉ ,3. K) : \n");
 scanf("%d", &t);
 
-if(t=1)
+if(t==1)
 {
 	printf("what '℃ '?\n");
 	scanf("%lf", &c);
 	printf("%lf ℃  is\n%lf ℉ \n%lf K",c, 1.8*c+32, c+273);
 }
-else if(t=2){
+else if(t==2){
 	printf("what '℉ '?\n");
 	scanf("%lf", &c);
 	printf("%lf ℉  is\n%lf ℃ \n%lf K",c, (c-32)/1.8, ((c-32)/1.8)+273);
 }
-else if(t=3){
+else if(t==3){
 	printf("what 'Kel'?\n");
 	scanf("%lf", &c);
 	printf("%lf K is\n%lf ℃ \n%lf ℉ ",c, c-273, ((c-273)*1.8+32));
